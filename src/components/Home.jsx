58678817484442642df3dc7d8d8959f24f6033ee
Jahan Shah <shahjahanrajan@gmail.com>
2023-12-09 03:39:29 +0530

    CSS, Home, Navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,26 @@
+import Img from './BG.jpg'
 
 const Home = () => {
     return(
-        <div>Home</div>
+        <div className="h-screen grid grid-rows-3 bg-center bg-cover bg-no-repeat" style={{ backgroundImage: `url(${Img})` }}>
+ <div className="row-span-1" />
+      <div className="row-span-1">
+        <div className="container mx-auto px-4">
+          <div className="w-full md:w-1/2">
+            <h1 className="text-5xl font-bold font-poppins text-left text-white mb-4">
+                Assuring Quality, Ensuring Security
+            </h1>
+            <p className="text-xl text-left text-gray-400 mb-8">
+            Revolutionizing smart contract security with AI-driven precision. Your trusted partner in navigating the complexities of blockchain technology.
+            </p>
+            {/*Button*/}
+            <button className='btn'> Launch App </button>
+	
+          </div>
+        </div>
+      </div>
+      <div className="row-span-1" />
+        </div>
     )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,24 @@
 
+
 const Navbar = () => {
     return(
-        <div>Navbar</div>
+        <div>
+        <nav className="fixed w-full top-0 z-50 bg-opacity-0">
+          <div className="container mx-auto px-6 py-4">
+            <div className="flex items-center justify-between">
+              <div className="text-white font-semibold font-poppins text-xl border p-1">
+                BlockAudit
+              </div>
+              <div className="flex space-x-4">
+                <a href="/" className="text-white">Home</a>
+                <a href="/Application" className="text-white">Application</a>
+  <div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
     )
 }
 
